fdf91a2c62ef4bea2d678466a972d7978730b399
SHINU VARGHESE <shinushinu01997@gmail.com>
2021-10-17 19:43:15 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state={
     db:null
 }
 module.exports.connect=function(done){
-    const url="mongodb://localhost:27017";
+    const url="mongodb+srv://shinuvarghese:shinu01997@cluster0.ind7u.mongodb.net/kikkimart?retryWrites=true&w=majority";
     const dbname='kikkimart'
 
     mongoClient.connect(url,{ useUnifiedTopology: true },(err,data)=>{
